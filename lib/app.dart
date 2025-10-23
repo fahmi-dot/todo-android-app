7b9dc914a30dce3246fabb4f60d2ae504bbf8cfe
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-24 04:54:57 +0700

    feat: create and setup app.dart and update main.dart

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'core/constants/app_strings.dart';
+import 'core/router/app_router.dart';
+import 'core/theme/app_theme.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppStrings.appName,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: AppRouter.todoList,
+    );
+  }
+}
