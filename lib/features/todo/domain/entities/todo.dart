1ae4d168f90044fbfe2c499c4b9c12c1bea8076a
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-24 12:33:35 +0700

    feat: add Todo entity

---
+++ lib/features/todo/domain/entities/todo.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+
+class Todo extends Equatable {
+  final String id;
+  final String title;
+  final bool isCompleted;
+  final DateTime createdAt;
+
+  const Todo({
+    required this.id,
+    required this.title,
+    required this.isCompleted,
+    required this.createdAt,
+  });
+
+  Todo copyWith({
+    String? id,
+    String? title,
+    bool? isCompleted,
+    DateTime? createdAt,
+  }) {
+    return Todo(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, title, isCompleted, createdAt];
+}
